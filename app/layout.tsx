52acbcd4c59d1cdd4f2b99ee716783d5ca2aa454
billychen0894 <billychen0894@gmail.com>
2023-11-19 16:36:24 -0800

    update: Rootlayout to have full height

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} antialiased h-full`}>
         {children}
         <Sunset />
       </body>
