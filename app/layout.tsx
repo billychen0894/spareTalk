0a561e44bd20651b85b4404151d4b6c009348a01
billychen0894 <billychen0894@gmail.com>
2023-11-21 21:08:46 -0800

    update: rootLayout w-full

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full w-full">
       <body
         className={`${inter.className} antialiased h-full bg-gradient-to-br from-indigo-400 via-purple-400 to-pink-400`}
       >
